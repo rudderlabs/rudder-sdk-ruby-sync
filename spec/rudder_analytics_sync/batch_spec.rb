704335ed76e7ed5b46666e5b907d888c5a2ed47a
Desu Sai Venkat <desusaivenkat@Desus-MacBook-Pro.local>
2023-01-24 18:32:13 +0530

    fix: fixed the un-defined keyword argument sent for request.post to support latest versions of Ruby SDK

--- spec/rudder_analytics_sync/batch_spec.rb
+++ spec/rudder_analytics_sync/batch_spec.rb
@@ -7,12 +7,12 @@ describe RudderAnalyticsSync::Batch do
 
   it 'supports identify, group, track and page' do
     request_stub = stub_request(:post, 'https://hosted.rudderlabs.com/v1/batch')
-                   .with do |request|
-                     batch = get_batch_from(request)
-                     batch.map do |operation|
-                       operation['type']
-                     end == %w[identify group track page]
-                   end
+                     .with do |request|
+      batch = get_batch_from(request)
+      batch.map do |operation|
+        operation['type']
+      end == %w[identify group track page]
+    end
 
     batch = described_class.new(client)
     batch.identify(user_id: 'id')
@@ -32,16 +32,16 @@ describe RudderAnalyticsSync::Batch do
     expected_context = {
       'foo' => 'bar',
       'library' => {
-        'name' => 'rudder-sdk-ruby-sync', 'version' => '2.0.0'
+        'name' => 'rudder-sdk-ruby-sync', 'version' => '2.0.1'
       }
     }
     request_stub = stub_request(:post, 'https://hosted.rudderlabs.com/v1/batch')
-                   .with do |request|
-                     batch = get_batch_from(request)
-                     batch.map do |operation|
-                       operation['context']
-                     end == [expected_context]
-                   end
+                     .with do |request|
+      batch = get_batch_from(request)
+      batch.map do |operation|
+        operation['context']
+      end == [expected_context]
+    end
 
     batch = described_class.new(client)
     batch.context = { 'foo' => 'bar' }
@@ -54,12 +54,12 @@ describe RudderAnalyticsSync::Batch do
   it 'allows to set common integrations' do
     expected_integrations = { 'foo' => 'bar' }
     request_stub = stub_request(:post, 'https://hosted.rudderlabs.com/v1/batch')
-                   .with do |request|
-                     batch = get_batch_from(request)
-                     batch.map do |operation|
-                       operation['integrations']
-                     end == [expected_integrations]
-                   end
+                     .with do |request|
+      batch = get_batch_from(request)
+      batch.map do |operation|
+        operation['integrations']
+      end == [expected_integrations]
+    end
 
     batch = described_class.new(client)
     batch.integrations = expected_integrations
@@ -83,12 +83,12 @@ describe RudderAnalyticsSync::Batch do
 
   it 'can be serialized and deserialized' do
     request_stub = stub_request(:post, 'https://hosted.rudderlabs.com/v1/batch')
-                   .with do |request|
-                     batch = get_batch_from(request)
-                     batch.map do |operation|
-                       operation['type']
-                     end == %w[identify track]
-                   end
+                     .with do |request|
+      batch = get_batch_from(request)
+      batch.map do |operation|
+        operation['type']
+      end == %w[identify track]
+    end
 
     batch = described_class.new(client)
     batch.identify(user_id: 'id')
